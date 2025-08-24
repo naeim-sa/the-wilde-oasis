39dea580fc66110f783f671288cf1625d0b53fd9
Naeim <naeim.smz@gmail.com>
2025-08-24 23:32:52 +0430

    created getCabin func and stractured data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
-import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import Heading from "../ui/Heading";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function functionName() {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter and Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
