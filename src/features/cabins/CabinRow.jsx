bf9b337f8603d4aad78214bc17e1d8ed1dcb3f60
Naeim <naeim.smz@gmail.com>
2025-08-25 16:43:13 +0430

    fetching cabin data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,6 @@
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +40,29 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guets</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+CabinRow.propTypes = {
+  cabin: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  maxCapacity: PropTypes.number.isRequired,
+  regularPrice: PropTypes.number.isRequired,
+  discount: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default CabinRow;
